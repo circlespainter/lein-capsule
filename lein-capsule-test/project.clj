1d409747f77de1a9eb5c8d52a2423298d9360a6b
circlespainter <ftudone@libero.it>
2014-09-04 11:20:33 +0200

    First revision of complete plugin comfiguration sample

--- lein-capsule-test/project.clj
+++ lein-capsule-test/project.clj
@@ -4,4 +4,173 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 	:plugins [[lein-capsule "0.1.0-SNAPSHOT"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+	;;; Capsule plugin configuration section, optional
+	:capsule {
+	;;; Optional
+		:min-plugin-version "0.1.0-SNAPSHOT"
+
+	;;; Optional
+		:log-level ""
+
+	;;; Optional, defaults to <jarbasename>-<capsuletype>-capsule.jar
+		:name "capsule.jar"
+
+	;;; Optional, only :this is default behaviour; if more than one type is configured, at least one must override :name
+		:types {
+			;; Optional, can override anything
+			:thin {
+			}
+			;; Optional, can override anything, will trigger building a fat capsule
+			:fat {
+			}
+		}
+
+	;;; Optional, check https://github.com/puniverse/capsule#application-id for defaults
+		:application {
+			;; Optional, check https://github.com/puniverse/capsule#application-id for defaults
+			:name "lein-capsule-test"
+			;; Optional, check https://github.com/puniverse/capsule#application-id for defaults
+			:version "0.1.0-SNAPSHOT"
+		}
+
+	;;; Optional, capsule modes, each of them can override anything except types and application settings
+		:modes {
+			:mode1 {
+				;; Optional, will ignore top-level settings and use this mode as default, defaults to false
+				:default false
+			}
+		}
+
+	;;; Optional
+		:execution {
+			;; Optional
+			:boot {
+				;; Optional, check https://github.com/puniverse/capsule#application-id for defaults
+				:main-class "Capsule"
+
+				;; Optional, check https://github.com/puniverse/capsule#capsules-cache for defaults
+				:extract-capsule false
+
+				;; Optional, if missing it'll be Leiningen's :main unless one of the following two is specified
+				:clojure-ns "main"
+
+				;; Optional, applicable only if application-ns nor Leiningen's main; if neither is present then
+				;; the next one must be
+				:scripts {
+					;; Mandatory
+					:unix ""
+					;; Mandatory
+					:windows ""
+				}
+
+				;; Optional, applicable only if none of the above is; in that case, if missing, the project's
+				;; complete artifact ID will be used
+				:artifact ""
+			}
+
+			:args []
+
+			;; Optional, check https://github.com/puniverse/capsule#selecting-the-java-runtime for defaults
+			:runtime {
+				;; Optional
+				:java-version ""
+				;; Optional
+				:min-java-version ""
+				;; Optional
+				:min-update-version ""
+				;; Optional
+				:jdk-required false
+
+				;; Optional
+				:jvm-args []
+
+				;; Optional
+				:system-properties {}
+
+				;; Optional
+				:environment-variables {}
+
+				;; Optional
+				:agents [
+					;; Optional
+					:embedded {
+						;; Mandatory
+						:jar ""
+						;; Optional
+						:params []
+					}
+					:artifact {
+						;; Mandatory
+						:id ""
+						;; Optional
+						:params []
+					}
+				]
+
+				:paths {
+					;; Optional
+					:embedded-classpath {
+						;; Optional
+						:override []
+					}
+
+					;; Optional
+					:boot-classpath {
+						;; Optional, will take precedence if the next one is present too
+						:prepend []
+						;; Optional
+						:override []
+					}
+
+					:native-library-path {
+						;; Optional, will take precedence if the next one is present too
+						:append []
+						;; Optional
+						:prepend []
+					}
+				}
+
+				:security {
+					:policy {
+						;; Optional, will take precedence if the next one is present too
+						:prepend ""
+						;; Optional
+						:override ""
+					}
+				}
+			}
+		}
+
+	;;; Optional, check https://github.com/puniverse/capsule#maven-dependencies for defaults
+		:dependencies {
+			;; Optional
+			:allow-snapshots false
+
+			;; Optional
+			:repositories [central]
+
+			;; Optional
+			:artifacts {
+				;; Optional
+				:jvm {
+					;; Optional
+					:add []
+					;; Optional
+					:remove []
+					;; Optional
+					:replace []
+				}
+				;; Optional
+				:native {
+					;; Optional
+					:windows []
+					;; Optional
+					:osx []
+					;; Optional
+					:linux []
+				}
+			}
+		}
+	})
--- src/leiningen/capsule.clj
+++ src/leiningen/capsule.clj
@@ -14,18 +14,41 @@
 						 (java.util.zip ZipFile ZipOutputStream ZipEntry)
 						 (org.apache.commons.io.output CloseShieldOutputStream)))
 
-(defn- strip-capsule-args [a]
-	(and
-		(not= a :fat)
-		(not= a :slim)))
+(defn- fill-capsule-manifest [project]
+	(->
+		project
+		(comment	; TODO Implement all
+			mf-put-execution
+			mf-put-application-id
+			mf-put-runtime
+			mf-put-cache-settings
+			mf-put-maven
+			mf-put-classpaths
+			mf-put-jvm
+			mf-put-security
+			mf-put-log)))
 
-(defn- build-capsule [project & args]
-	(main/info "Unimplemented (yet)"))	; TODO Implement
+(defn- fill-capsule-dependency [project]
+	(update-in project [:dependencies]
+		(fn [deps]
+			project))) ; TODO Implement
+
+(defn- capsulize [project]
+	(->
+		project
+		fill-capsule-dependency
+		fill-capsule-manifest))
+
+(defn- build-capsule [project]
+	(let [
+		project (project/merge-profiles project [:capsule])
+		project (capsulize project)
+	 ]
+		(main/info "Unimplemented (yet)"))) ; TODO Implement
 
 (defn capsule
 	"Creates a capsule for the project."
-	([project & args]
-	 (apply compile/compile (cons project (filter strip-capsule-args args)))
+	[project & args]
+	 (apply compile/compile (cons project args))
 	 ; Middleware will have already filled leiningen's manifest map appropriately
-	 (apply build-capsule (cons project args)))	; TODO Implement
-	([project] (capsule project :fat))) ; Default is fat capsule
+	 (apply build-capsule [project]))
